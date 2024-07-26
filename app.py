8f6d22e9ddedb0a86391da05190f8645ee22dc95
Mannopboy <mannopboy8@gmail.com>
2024-07-26 19:47:53 +0500

    added Qatiyatlilikni baholash testi and Siz qanchalik sabrlisiz

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from backend.models.models import *
+from backend.functions.infos import *
 
 app = Flask(__name__)
 app.config.from_object('backend.models.config')
@@ -7,71 +8,9 @@ db = db_setup(app)
 migrate = Migrate(app, db)
 
 
-questions = [
-    "Kelajakdagi maqsadimni belgilab oldim va unga erishishga hozirlik ko'ryapman",
-    "Qanchalik uzoq bo'lishidan qat'i nazar, men ko'zlangan maqsad sari intilaman",
-    "Agar biror narsa uzoq muddatli maqsadni amalga oshirishga xalaqit bersa, men odatda uzoq maqsadga erishish istagini yo'qotaman",
-    "Muvaffaqiyatsizliklar bo'lsa ham, men o'z maqsadimga erishisha olishimga ishonchim komil",
-    "Men o'z oldimga juda uzoq maqsadlar qo'ymaslikka harakat qilaman, chunki bugungi kun bilan yashash osonroq deb o'ylayman",
-    "Men bir necha bor o'zimni rivojlantirishga harakat qildim, lekin hech narsa chiqmadi",
-    "Muvaffaqiyatsizliklar meni maqsadga erishish yo’lidan chetlatadi va men biron bir muhim narsaga erishish niyatidan voz kechaman.",
-    "Agar men o’z oldimga o'zim uchun muhim bo'lgan maqsadni qo'ygan bo'lsam, unda meni to'xtatish qiyin.",
-    "Mag'lubiyat meni yanada yangi kuch bilan harakat qilishga undaydi",
-    "Men haftalikni ishlarimni rejalashtirishga ko'p marta urinib ko'rdim, lekin rejani noto’g’ri tashkil qilganim uchun hech qachon rejalashtirganimni amalga oshira olmadim.",
-    "Qiyinchiliklar paydo bo'lganda, men boshlagan ishimni davom ettirishga arziydimi yoki yo'qligiga shubha qila boshlayman.",
-    "Menga ko'pincha ishlarni oxirigacha bajarish qiyin, ayniqsa bunga haftalar yoki oylar kerak bo'lganda.",
-    "Mening yaqinlarim meni biror narsaga qaramlikka moyil deb o'ylashadi",
-    "Mavjud qiyinchiliklarga qaramay, maqsadlarimga erishganimda katta mamnuniyat his qilaman.",
-    "Ko'pincha men boshlagan narsalarni yarim yo'lda tashlab qo’yaman va ularga qiziqishimni yo'qotaman.",
-    "Men kutish va chidashni bilaman, shuning uchun uzoq maqsadlar meni qo'rqitmaydi.",
-    "To'siqlar meni faqat harakatga undaydi va qarorlarimni kuchliroq qiladi.",
-    "Muvaffaqiyatga erishishga bo’lgan shubha emas, balki dangasalik ko’pincha meni maqsadlarimga erishishdan voz kechishga majbur qiladi."
-]
-
-positive_indices = {0, 1, 3, 7, 8, 12, 13, 15, 16}
-negative_indices = {2, 4, 5, 6, 9, 10, 11, 14, 17}
-
-
-@app.route('/')
-def index():
-    test_info = [
-        {
-            "name": "Maqsadga intiluvchanlik",
-            "variants": [
-                {
-                    "name": 6,
-                    "desc": "6 ballgacha – sizda maqsadga intiluvchanlikning past darajasi aniqlandi. Sizning yumshoq ko’ngilligingiz va ko’pincha boshqalarga yon berishingiz bunga sabab bo’lishi mumkin. Biroq shuni esda tutinki, qat’iyatlilik, maqsadga intiluvchanlik xislatlarini o’zingizda shakllantira olsangiz o’qishda, ishda muvaffaqiyatlarga erisha olasiz."
-                },
-                {
-                    "name": 7,
-                    "desc": "7-11 ballgacha – Sizda maqsadga intiluvchanlikning o’rta darajasi. Biroq bu sizning qo’lingizdan ko’p narsa kelmaydi degani emas. Aksincha o’qishga, ishga, maqsadingizga nisbatan me’yorda yondashish sizni boshqalardan ajratib turadi."
-                },
-                {
-                    "name": 12,
-                    "desc": "Sizda maqsadga intiluvchanlikning yuqori darajasi. Siz barcha boshlagan ishlaringizda oldingizga qo’ygan maqsadingizga intilishda tinimsiz mehnat qilasiz."
-                },
-            ],
-            "desc": "Ushbu metodika  E. P. Ilin va E. K. Feshchenko tomonidan ishlab chiqilgan. Ko'rsatmalar: Sizga bir nechta savollar taqdim etiladi. Ushbu vaziyatlarga o'zingizni qo’yib ko’ring  va ular siz uchun qanchalik mos ekanligini baholang. Agar siz ushbu fikrga rozi bo'lsangiz, '+' belgisini qo'ying, agar rozi bo'lmasangiz, '-' belgisini qo'ying."
-        },
-        {
-            "name": "Siz qanchalik sabrlisiz",
-            "desc": "",
-            "variants": [
-                {
-                    "name": 4,
-                    "desc": "Siz qatyatlisiz va o'jarsiz. Qaerda bo'lishingizdan  qat'iy nazar, siz fikringizni boshqalarga yuklamoqchi bo'lgandek ta'ssurot qoldirishingiz mumkin, ko'pincha ikkilanmasdan, maqsadingizga yordam berish uchun siz tez-tez ovozingizni ko'tarasiz. Sizning xarakteringiz sizdan boshqacha fikrlaydigan va siz aytayotgan va qiladigan ishlaringizga rozi bo'lmagan odamlar bilan normal munosabatlarni saqlashni qiyinlashtirish."
-                },
-                {
-                    "name": 14,
-                    "desc": "Siz o'z fikringizni qat'iy himoya qilishga qodirsiz. Lekin, albatta, siz ham muloqot jarayonida agar kerak bo'lsa, fikringizni o'zgartirishingiz mumkin. Sizda ba'zan haddan tashqari keskinlik, suxbatdoshga hurmatsizlik ko'rsatishingiz ham mumkin. Va bunday payda siz xaqiqatan ham zaifroq xarakterga ega bo'lgan odam bilan baxsda g'alaba qozonishingiz mumkin."
-                },
-                {
-                    "name": 18,
-                    "desc": "Sizning etiqodlaringizning qat'ijligi ajoyib noziklik, ongingizning qobiliyatiga bilan mukammal birlashtirilgan. Siz har qanday g'oyalarni qabul qilish va qarashda, birinchi ishni boshlashingiz mumkin. Siz notug'ri bo'lgan fikrlarni hurmat va hushmuomalalik bilan rad eta olasiz."
-                }
-            ]
-        }
-    ]
+@app.route('/', defaults={"test_id": 1})
+@app.route('/<int:test_id>')
+def index(test_id):
     for info in test_info:
         test_info_add = TestInfo.query.filter(TestInfo.name == info['name']).first()
         if not test_info_add:
@@ -84,7 +23,19 @@ def index():
                 test_options_add = TestAnswerOptions(name=test_options['name'], test_info_id=test_info_add.id,
                                                      desc=test_options['desc'])
                 test_options_add.add()
-    return render_template('index.html', questions=questions)
+    test = TestInfo.query.filter(TestInfo.id == test_id).first()
+    if test.name == 'Maqsadga intiluvchanlik':
+        questions = questions_purpose
+    elif test.name == 'Qat’iyatlilikni baholash testi':
+        questions = questions_persistence
+    elif test.name == 'Siz qanchalik sabrlisiz':
+        questions = questions_patience
+    else:
+        questions = []
+    return render_template('index.html', questions=questions, selected_test=test,
+                           tests=[{'id': test.id, 'name': test.name} for test in
+                                  TestInfo.query.order_by(TestInfo.id).all()]
+                           )
 
 
 @app.route('/submit', methods=['POST'])
@@ -93,38 +44,41 @@ def submit():
     answers = data['answers']
     age = data['age']
     gender = data['gender']
-    test_info = TestInfo.query.filter(TestInfo.name == "Maqsadga intiluvchanlik").first()
-
-    # txt = "h3110 23 cat 444.4 rabbit 11 2 dog"
-    # [int(s) for s in txt.split() if s.isdigit()]
+    test = data['test']
+    test_info = TestInfo.query.filter(TestInfo.id == test).first()
     user = User(age=age, gender=gender)
     user.add()
     score = 0
-    for i, answer in enumerate(answers):
-        if answer == '+':
-            if i in positive_indices:
-                score += 1
-        elif answer == '-':
-            if i in negative_indices:
-                score += 1
+    for answer in answers:
+        score += int(answer)
+    test_options = TestAnswerOptions.query.filter(TestAnswerOptions.test_info_id == test_info.id).order_by(
+        TestAnswerOptions.id).all()
+    for test_option in test_options:
+        if test_info.name == 'Maqsadga intiluvchanlik':
+            if 6 >= score > 0:
+                option = test_option
+            elif 7 <= score <= 11:
+                option = test_option
+            else:
+                option = test_option
+        elif test_info.name == 'Qat’iyatlilikni baholash testi':
+            if 6 >= score > 0:
+                option = test_option
+            elif 7 <= score <= 11:
+                option = test_option
+            else:
+                option = test_option
+        elif test_info.name == 'Siz qanchalik sabrlisiz':
+            if 4 >= score > 0:
+                option = test_option
+            elif 4 <= score <= 14:
+                option = test_option
+            else:
+                option = test_option
 
-    if 6 >= score > 0:
-        test_options = TestAnswerOptions.query.filter(
-            and_(TestAnswerOptions.name <= 6, TestAnswerOptions.name > 0),
-            TestAnswerOptions.test_info_id == test_info.id).first()
-        result = "Sizda maqsadga intiluvchanlikning past darajasi aniqlandi. Sizning yumshoq ko’ngilligingiz va ko’pincha boshqalarga yon berishingiz bunga sabab bo’lishi mumkin. Biroq shuni esda tutinki, qat’iyatlilik, maqsadga intiluvchanlik xislatlarini o’zingizda shakllantira olsangiz o’qishda, ishda muvaffaqiyatlarga erisha olasiz. "
-    elif 7 <= score <= 11:
-        test_options = TestAnswerOptions.query.filter(
-            and_(TestAnswerOptions.name <= 11, TestAnswerOptions.name > 6),
-            TestAnswerOptions.test_info_id == test_info.id).first()
-        result = "Sizda maqsadga intiluvchanlikning o’rta darajasi. Biroq bu sizning qo’lingizdan ko’p narsa kelmaydi degani emas. Aksincha o’qishga, ishga, maqsadingizga nisbatan me’yorda yondashish sizni boshqalardan ajratib turadi."
-    else:
-        test_options = TestAnswerOptions.query.filter(
-            TestAnswerOptions.name >= 12, TestAnswerOptions.test_info_id == test_info.id).first()
-        result = "Sizda maqsadga intiluvchanlikning yuqori darajasi. Siz barcha boshlagan ishlaringizda oldingizga qo’ygan maqsadingizga intilishda tinimsiz mehnat qilasiz.  "
-    test = Test(test_info_id=test_info.id, answer_id=test_options.id, user_id=user.id)
+    test = Test(test_info_id=test_info.id, answer_id=option.id, user_id=user.id)
     test.add()
-    return jsonify(score=score, result=result)
+    return jsonify(score=score, result=option.desc)
 
 
 if __name__ == '__main__':
---
+++ backend/functions/infos.py
@@ -0,0 +1,285 @@
+questions_patience = [
+    {'question': "Siz uchun yoqimsiz bo'lgan yangilikni xotirjamlik bilan qabul qilasizmi?", 'answers': [
+        {'name': "ha", 'value': 0},
+        {'name': "yo'q", 'value': 2},
+    ], 'index': 1},
+    {
+        'question': "Tasavvur qiling siz do'stlaringiz bilan ko'rgandingiz va kimdir o'yin o'ynashni taklif qiladi. Siz nimani afzal ko'rasiz?",
+        'answers': [
+            {'name': "faqat yaxshi o'ynaganlar ishtirok etishi", 'value': 0},
+            {'name': "o'ynash qoidalarini bilmaganlar o'ynashini", 'value': 2},
+        ], 'index': 2},
+    {
+        'question': "Sizda qiziqarli fikr bor deb o'ylaysiz, lekin uni hech kim qo'llab-quvvatlamadi. Siz xafa bo'lasizmi?",
+        'answers': [
+            {'name': "ha", 'value': 0},
+            {'name': "yo'q", 'value': 2},
+        ], 'index': 3},
+    {'question': "Jamoat joylarida mast bo'lib ko'rinadigan odamlar sizni bezovta qiladimi?", 'answers': [
+        {'name': "agar ular maqbul chegalarni kesib o'tmasalar, men bunga umuman qiziqmayman", 'value': 2},
+        {'name': "men har doim o'zini tuta olmaydigan odamlarni yoqtirmaganman", 'value': 0},
+    ], 'index': 4},
+    {'question': "Boshqa kasb, lavozim, urf-odat egalari bilan aloqani osongina topa olasizmi?", 'answers': [
+        {'name': "buni qilish men uchun qiyin bo'lardi", 'value': 0},
+        {'name': "men bunday narsalarga e'tibor bermayman", 'value': 2},
+    ], 'index': 5},
+    {'question': "Siz obyekti bo'lgan hazilga qanday munosabatda bo'lasiz?", 'answers': [
+        {'name': "mengaga hazillarning o'zi yoki hazilkashlar yoqmaydi", 'value': 0},
+        {
+            'name': "Shunchaki hazil bo'lsa ham men uchun yoqimsiz bo'lsa, men ham xuddi shunday javob berishga harakat qilaman",
+            'value': 2},
+    ], 'index': 6},
+    {'question': "Ko'p odamlar 'notog'ri joyda o'tirishadi', 'o'z ishini qilmaydilar'degan fikrga qo'shilasizmi?",
+     'answers': [
+         {'name': "ha", 'value': 0},
+         {'name': "yo'q", 'value': 2},
+     ], 'index': 7},
+    {
+        'question': "Siz davraga do'stingizni (qiz do'stingizni) olib kelasiz, va u hammaning e'tiboriga aylanishadi. Bunga qanday munosabatdasiz?",
+        'answers': [
+            {
+                'name': "rostini aytsam, hammaning e'tibori mendan shu tarzda boshqa insonga yo'natilsa men uchun yoqimsiz",
+                'value': 0},
+            {'name': "men u uchun xursandman", 'value': 2},
+        ], 'index': 8},
+    {
+        'question': "Bir ziyofatda siz zamonaviy yosh avlodni tanqid qiladigan, eski kunlarni maqtaydigan keksa odamni uchratasiz. Siz qanday munosabatda bo'lasiz?",
+        'answers': [
+            {'name': "ishonchli bahona bilan erta ketasiz", 'value': 2},
+            {'name': "u inson bilan tortishasiz", 'value': 0},
+        ], 'index': 9}
+]
+
+questions_purpose = [
+    {'question': "Kelajakdagi maqsadimni belgilab oldim va unga erishishga hozirlik ko'ryapman", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 1},
+    {'question': "Qanchalik uzoq bo'lishidan qat'i nazar, men ko'zlangan maqsad sari intilaman", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 2},
+    {
+        'question': "Agar biror narsa uzoq muddatli maqsadni amalga oshirishga xalaqit bersa, men odatda uzoq maqsadga erishish istagini yo'qotaman",
+        'answers': [
+            {'name': 'Ha', 'value': 0},
+            {'name': "Yo'q", 'value': 1},
+        ], 'index': 3},
+    {'question': "Muvaffaqiyatsizliklar bo'lsa ham, men o'z maqsadimga erishisha olishimga ishonchim komil",
+     'answers': [
+         {'name': 'Ha', 'value': 1},
+         {'name': "Yo'q", 'value': 0},
+     ], 'index': 4},
+    {
+        'question': "Men o'z oldimga juda uzoq maqsadlar qo'ymaslikka harakat qilaman, chunki bugungi kun bilan yashash osonroq deb o'ylayman",
+        'answers': [
+            {'name': 'Ha', 'value': 0},
+            {'name': "Yo'q", 'value': 1},
+        ], 'index': 5},
+    {'question': "Men bir necha bor o'zimni rivojlantirishga harakat qildim, lekin hech narsa chiqmadi", 'answers': [
+        {'name': 'Ha', 'value': 0},
+        {'name': "Yo'q", 'value': 1},
+    ], 'index': 6},
+    {
+        'question': "Muvaffaqiyatsizliklar meni maqsadga erishish yo’lidan chetlatadi va men biron bir muhim narsaga erishish niyatidan voz kechaman.",
+        'answers': [
+            {'name': 'Ha', 'value': 0},
+            {'name': "Yo'q", 'value': 1},
+        ], 'index': 7},
+    {'question': "Agar men o’z oldimga o'zim uchun muhim bo'lgan maqsadni qo'ygan bo'lsam, unda meni to'xtatish qiyin.",
+     'answers': [
+         {'name': 'Ha', 'value': 1},
+         {'name': "Yo'q", 'value': 0},
+     ], 'index': 8},
+    {'question': "Mag'lubiyat meni yanada yangi kuch bilan harakat qilishga undaydi", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 9},
+    {
+        'question': "Men haftalikni ishlarimni rejalashtirishga ko'p marta urinib ko'rdim, lekin rejani noto’g’ri tashkil qilganim uchun hech qachon rejalashtirganimni amalga oshira olmadim.",
+        'answers': [
+            {'name': 'Ha', 'value': 0},
+            {'name': "Yo'q", 'value': 1},
+        ], 'index': 10},
+    {
+        'question': "Qiyinchiliklar paydo bo'lganda, men boshlagan ishimni davom ettirishga arziydimi yoki yo'qligiga shubha qila boshlayman.",
+        'answers': [
+            {'name': 'Ha', 'value': 0},
+            {'name': "Yo'q", 'value': 1},
+        ], 'index': 11},
+    {
+        'question': "Menga ko'pincha ishlarni oxirigacha bajarish qiyin, ayniqsa bunga haftalar yoki oylar kerak bo'lganda.",
+        'answers': [
+            {'name': 'Ha', 'value': 0},
+            {'name': "Yo'q", 'value': 1},
+        ], 'index': 12},
+    {'question': "Mening yaqinlarim meni biror narsaga qaramlikka moyil deb o'ylashadi", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 13},
+    {'question': "Mavjud qiyinchiliklarga qaramay, maqsadlarimga erishganimda katta mamnuniyat his qilaman.",
+     'answers': [
+         {'name': 'Ha', 'value': 1},
+         {'name': "Yo'q", 'value': 0},
+     ], 'index': 14},
+    {'question': "Ko'pincha men boshlagan narsalarni yarim yo'lda tashlab qo’yaman va ularga qiziqishimni yo'qotaman.",
+     'answers': [
+         {'name': 'Ha', 'value': 0},
+         {'name': "Yo'q", 'value': 1},
+     ], 'index': 15},
+    {'question': "Men kutish va chidashni bilaman, shuning uchun uzoq maqsadlar meni qo'rqitmaydi.", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 16},
+    {'question': "To'siqlar meni faqat harakatga undaydi va qarorlarimni kuchliroq qiladi.", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 17},
+    {
+        'question': "Muvaffaqiyatga erishishga bo’lgan shubha emas, balki dangasalik ko’pincha meni maqsadlarimga erishishdan voz kechishga majbur qiladi.",
+        'answers': [
+            {'name': 'Ha', 'value': 0},
+            {'name': "Yo'q", 'value': 1},
+        ], 'index': 18}
+]
+questions_persistence = [
+    {'question': "Agar qiyin ishlarni bajarib charchasam, darhol uni tashlab qo'yaman", 'answers': [
+        {'name': 'Ha', 'value': 0},
+        {'name': "Yo'q", 'value': 1},
+    ], 'index': 1},
+    {'question': "Zerikarli bo’lgan hikoyalarni oxirgacha o’qish uchun mening sabrim yetmaydi", 'answers': [
+        {'name': 'Ha', 'value': 0},
+        {'name': "Yo'q", 'value': 1},
+    ], 'index': 2},
+    {'question': "Men uzoq navbatda turishni yomon ko’raman va ko’pincha navbatning oxirigacha turmasdan ketib qolaman",
+     'answers': [
+         {'name': 'Ha', 'value': 0},
+         {'name': "Yo'q", 'value': 1},
+     ], 'index': 3},
+    {'question': "Men og’riqlarga uzoq vaqtgacha chiday olaman, masalan tish og’rig’i", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 4},
+    {'question': "Odatda men uzoq vaqtgacha chanqoqlikka (tashnalikka) chiday olaman", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 5},
+    {
+        'question': "Men uzoq vaqtgacha bo’ladigan ochlikka chiday olmasdim, misol uchun ozish uchun yoki davolanish uchun",
+        'answers': [
+            {'name': 'Ha', 'value': 0},
+            {'name': "Yo'q", 'value': 1},
+        ], 'index': 6},
+    {'question': "Agar men jismoniy tarbiya darsida charchab qolsam, ko’pincha mashqlarni bajarishni to’xtataman",
+     'answers': [
+         {'name': 'Ha', 'value': 0},
+         {'name': "Yo'q", 'value': 1},
+     ], 'index': 7},
+    {'question': "Men kamdan kam hollarda zerikarli bo’lgan ishlarni tugatmasdan tashlab qo’yaman.", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 8},
+    {'question': "Odatda, o'zimni 'qo'limdan kelmaydigan' ishlarga majburlash men uchun qiyin", 'answers': [
+        {'name': 'Ha', 'value': 0},
+        {'name': "Yo'q", 'value': 1},
+    ], 'index': 9},
+    {'question': "Charchaganimga qaramay, ishni yarim yo‘lda tashlab ketmayman.", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 10},
+    {'question': "Menga topshiriqni bajarish uchun o'zimni majburlashim kerak bo'lgan jismoniy ish yoqadi.",
+     'answers': [
+         {'name': 'Ha', 'value': 1},
+         {'name': "Yo'q", 'value': 0},
+     ], 'index': 11},
+    {'question': "Ishonch bilan aytishim mumkinki, men sabrliman", 'answers': [
+        {'name': 'Ha', 'value': 1},
+        {'name': "Yo'q", 'value': 0},
+    ], 'index': 12},
+    {
+        'question': "Charchoqqa qaramay, yugurish paytida yuqori tempni saqlab qolish uchun bor kuchim bilan harakat qilaman.",
+        'answers': [
+            {'name': 'Ha', 'value': 1},
+            {'name': "Yo'q", 'value': 0},
+        ], 'index': 13},
+    {'question': "Avtobus bekatida garchi shoshmasamda avtobus kutish g’ashimni keltiradi", 'answers': [
+        {'name': 'Ha', 'value': 0},
+        {'name': "Yo'q", 'value': 1},
+    ], 'index': 14},
+    {'question': "Men og’riqlarga chidamsizman", 'answers': [
+        {'name': 'Ha', 'value': 0},
+        {'name': "Yo'q", 'value': 1},
+    ], 'index': 15},
+    {
+        'question': "Men o'zimni zaif irodali deb hisoblashni xohlamayman, shuning uchun har safar og'ir jismoniy ishlarni oxirigacha bajarishga harakat qilaman",
+        'answers': [
+            {'name': 'Ha', 'value': 1},
+            {'name': "Yo'q", 'value': 0},
+        ], 'index': 16},
+    {'question': "Men printsip bo'yicha harakat qilaman: 'Agar biror ishni boshlagan bo’lsang, uni oxirigacha yetkaz'.",
+     'answers': [
+         {'name': 'Ha', 'value': 1},
+         {'name': "Yo'q", 'value': 0},
+     ], 'index': 17},
+    {
+        'question': "Men 'sabr-toqat va mehnat hamma narsani yengishiga' ishonmayman. Inson aql bilan ish ko’rishi kerak, charchoq bilan emas",
+        'answers': [
+            {'name': 'Ha', 'value': 0},
+            {'name': "Yo'q", 'value': 1},
+        ], 'index': 18}
+]
+test_info = [
+    {
+        "name": "Maqsadga intiluvchanlik",
+        "variants": [
+            {
+                "name": 6,
+                "desc": "Sizda maqsadga intiluvchanlikning past darajasi aniqlandi. Sizning yumshoq ko’ngilligingiz va ko’pincha boshqalarga yon berishingiz bunga sabab bo’lishi mumkin. Biroq shuni esda tutinki, qat’iyatlilik, maqsadga intiluvchanlik xislatlarini o’zingizda shakllantira olsangiz o’qishda, ishda muvaffaqiyatlarga erisha olasiz."
+            },
+            {
+                "name": 7,
+                "desc": "Sizda maqsadga intiluvchanlikning o’rta darajasi. Biroq bu sizning qo’lingizdan ko’p narsa kelmaydi degani emas. Aksincha o’qishga, ishga, maqsadingizga nisbatan me’yorda yondashish sizni boshqalardan ajratib turadi."
+            },
+            {
+                "name": 12,
+                "desc": "Sizda maqsadga intiluvchanlikning yuqori darajasi. Siz barcha boshlagan ishlaringizda oldingizga qo’ygan maqsadingizga intilishda tinimsiz mehnat qilasiz."
+            },
+        ],
+        "desc": "Ushbu metodika  E. P. Ilin va E. K. Feshchenko tomonidan ishlab chiqilgan. Ko'rsatmalar: Sizga bir nechta savollar taqdim etiladi. Ushbu vaziyatlarga o'zingizni qo’yib ko’ring  va ular siz uchun qanchalik mos ekanligini baholang. Agar siz ushbu fikrga rozi bo'lsangiz, '+' belgisini qo'ying, agar rozi bo'lmasangiz, '-' belgisini qo'ying."
+    },
+    {
+        "name": "Qat’iyatlilikni baholash testi",
+        "desc": "Ushbu metodika E. P. Ilin va E. K. Feshchenko tomonidan ishlab chiqilgan. ",
+        "variants": [
+            {
+                "name": 6,
+                "desc": "Sizda qat’iyatlilikning past darajasi aniqlandi. Qat’iyatlilikning rivojlanmaganligi sizda o’z oldingizga qo’ygan maqsadlaringizga erishishda qiyinchiliklarni yuzaga keltiradi. Lekin agar siz hozirdan o’zingizdagi qat’iyatlilikni oshirish uchun o’z ustingizda ishlasangiz barcha muammolardan chiqish imkoniyatiga ega bo’lasiz"
+            }, {
+                "name": 11,
+                "desc": ""
+            }, {
+                "name": 18,
+                "desc": "Sizning qat’iyatlilik darajangiz yuqori ekanligi aniqlandi. Qat’iyatlilik yaxshi rivojlanganda siz har qanday qiyinchiliklarni yenga olasiz. Chunki bu qiyinchiliklarni yengish uchun sizda qat’iyatlilik bor."
+            },
+
+        ]
+    },
+    {
+        "name": "Siz qanchalik sabrlisiz",
+        "desc": "",
+        "variants": [
+            {
+                "name": 4,
+                "desc": "Siz qatyatlisiz va o'jarsiz. Qaerda bo'lishingizdan  qat'iy nazar, siz fikringizni boshqalarga yuklamoqchi bo'lgandek ta'ssurot qoldirishingiz mumkin, ko'pincha ikkilanmasdan, maqsadingizga yordam berish uchun siz tez-tez ovozingizni ko'tarasiz. Sizning xarakteringiz sizdan boshqacha fikrlaydigan va siz aytayotgan va qiladigan ishlaringizga rozi bo'lmagan odamlar bilan normal munosabatlarni saqlashni qiyinlashtirish."
+            },
+            {
+                "name": 14,
+                "desc": "Siz o'z fikringizni qat'iy himoya qilishga qodirsiz. Lekin, albatta, siz ham muloqot jarayonida agar kerak bo'lsa, fikringizni o'zgartirishingiz mumkin. Sizda ba'zan haddan tashqari keskinlik, suxbatdoshga hurmatsizlik ko'rsatishingiz ham mumkin. Va bunday payda siz xaqiqatan ham zaifroq xarakterga ega bo'lgan odam bilan baxsda g'alaba qozonishingiz mumkin."
+            },
+            {
+                "name": 18,
+                "desc": "Sizning etiqodlaringizning qat'ijligi ajoyib noziklik, ongingizning qobiliyatiga bilan mukammal birlashtirilgan. Siz har qanday g'oyalarni qabul qilish va qarashda, birinchi ishni boshlashingiz mumkin. Siz notug'ri bo'lgan fikrlarni hurmat va hushmuomalalik bilan rad eta olasiz."
+            }
+        ]
+    },
+]
