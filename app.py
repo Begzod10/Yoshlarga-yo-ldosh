63012076d836ac0fbef50cfdfee4f9d0ac472a40
Mannopboy <mannopboy8@gmail.com>
2024-07-30 16:05:48 +0500

    asdsadd

--- app.py
+++ app.py
@@ -48,6 +48,17 @@ def index(test_id):
                            )
 
 
+def calculate_section_scores(questions, sections):
+    section_scores = {section: 0 for section in sections}
+    for q in questions:
+        question_number = int(q['question'])
+        value = int(q['value'])
+        for section, numbers in sections.items():
+            if question_number in numbers:
+                section_scores[section] += value
+    return section_scores
+
+
 @app.route('/submit', methods=['POST'])
 def submit():
     data = request.get_json()
@@ -133,7 +144,7 @@ def submit():
                 and_(TestAnswerOptions.name <= 10, TestAnswerOptions.name > 0),
                 TestAnswerOptions.test_info_id == test_info.id).first()
         elif 10 <= score <= 20:
-             test_option = TestAnswerOptions.query.filter(
+            test_option = TestAnswerOptions.query.filter(
                 and_(TestAnswerOptions.name <= 20, TestAnswerOptions.name > 10),
                 TestAnswerOptions.test_info_id == test_info.id).first()
         test = Test(test_info_id=test_info.id, answer=test_option.desc, user_id=user.id)
@@ -159,8 +170,27 @@ def submit():
         test = Test(test_info_id=test_info.id, answer=test_option.desc, user_id=user.id)
         test.add()
         results.append(test_option.desc)
-    elif test_info.name == "IPM / ijtimoiy – psixologik maslashganlik ":
-        pass
+    elif test_info.name == "IPM / ijtimoiy – psixologik maslashganlik / so‘rovnomasi":
+        sections = {
+            'Moslashganlik': [4, 5, 9, 12, 15, 19, 22, 23, 26, 27, 29, 33, 35, 37, 41, 44, 47, 51, 53, 55, 61, 63, 67,
+                              72, 74, 75, 78, 80, 88, 91, 94, 96, 97, 98],
+            'Moslashmaganlik': [2, 6, 8, 13, 16, 18, 25, 28, 32, 36, 38, 40, 42, 43, 49, 50, 54, 56, 59, 60, 62, 64, 69,
+                                71, 73, 76, 77, 83, 84, 86, 90, 95, 99, 100],
+            'O‘z-o‘zini qabul qilish': [33, 35, 55, 67, 72, 74, 75, 80, 88, 94, 96],
+            'O‘z-o‘zini qabul qilmaslik': [7, 59, 62, 65, 90, 95, 99],
+            'Boshqalarni qabul qilish': [9, 14, 22, 26, 53, 97],
+            'Boshqalarni qabul qilmaslik': [2, 10, 21, 28, 40, 60, 76],
+            'Hissiy qulaylik': [23, 29, 30, 41, 44, 47, 78],
+            'Hissiy noqulaylik': [6, 42, 43, 49, 50, 83, 85],
+            'Ichki nazorat': [4, 5, 11, 12, 19, 29, 37, 51, 63, 68, 79, 91, 98],
+            'Tashqi nazorat': [13, 25, 36, 52, 57, 70, 71, 73, 77],
+            'Ustuvorlik': [58, 61, 66],
+            'Ergashuvchanlik': [16, 32, 38, 69, 84, 87],
+            'Eskalizm': [17, 18, 54, 64, 86]
+        }
+        scores = calculate_section_scores(answers, sections)
+        if 0 < scores['Moslashganlik'] <= 170:
+            pass
     return jsonify(score=score, results=results)
 
 
